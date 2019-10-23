e1d37c19dbdc18a0cec36379f25b4adc7f78de61
sanselena <53650879+sanselena@users.noreply.github.com>
2019-10-23 12:37:34 +0300

    Update CodeKAL sevgisi.py

--- Python/CodeKAL sevgisi.py
+++ Python/CodeKAL sevgisi.py
@@ -3,6 +3,11 @@
 print("CodeKAL'ı çok seviyorum")
 print("CodeKAL'ın bir parçası olmaktan çok mutluyum!")
 
+print("I love CodeKAL!!")
+print("Im so happy to be a part of it.")
+
+#java daha komplike, sadece ekrana yazdırma komutunun bile daha uzun olması, noktalı virgüller...
+
 #GÖREV: Yukarıdaki komutların benzerin kullanarak İngilizce çıktılarını veriniz.
 
 #YAZ: Aynı görevi java programlama dili ile de yaptınız. Bu iki dil arasında gördüğünüz farklar nedir?
