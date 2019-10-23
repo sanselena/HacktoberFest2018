e091369a460db2a0cd93f5de70e0936ac19e3ebc
sanselena <53650879+sanselena@users.noreply.github.com>
2019-10-23 12:35:40 +0300

    Update Litre Dönüştürücü.py

--- Python/Litre Dönüştürücü.py
+++ Python/Litre Dönüştürücü.py
@@ -2,14 +2,15 @@
 #GÖREV: Baş bırakılan yerleri doldurunuz:
 #Girdileri alıyoruz:
 kenar1 = int(input("1.kenarı giriniz:"))
-kenar2 = #Kodu tamamlayınız
-kenar3 = #Kodu tamamlayınız
+kenar2 = int(input("2.kenarı giriniz:"))
+kenar3 = int(input("3.kenarı giriniz:"))
 
 #Prizmanın hacmini cm^3 cinsinden giriniz:
-hacimCM3 = #Kodu tamamlayınız
+hacimCM3 = kenar1 * kenar2 * kenar3
 
 #Prizmanın hacmini litre cinsinden heaplıyoruz:
-hacimL = #Kodu tamamlayınız
+hacimL = hacimCM3 / 1000;
 
 #Çıktıyı alıyoruz:
 print("Litre cinsinden hacmimiz: ", hacimL)
+print("sanitmetre küp cinsinden hacminiz: ",hacimCM3)
